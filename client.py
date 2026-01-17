980d675ecfacd145a738d3fb9f0ad1b279073084
Balbino Aylagas <38702996+baylagas@users.noreply.github.com>
2026-01-17 11:43:41 -0600

    add logic methods

--- client.py
+++ client.py
@@ -4,6 +4,14 @@ class Client:
         self.name = name
         self.age = age
         self.monthly_salary = monthly_salary
+        self.yearly_salary = self.get_yearly_salary()
+        self.loan_approved = self.get_loan_approval()
+
+    def get_yearly_salary(self) -> float:
+        return self.monthly_salary * 12
+
+    def get_loan_approval(self) -> bool:
+        return self.yearly_salary > 3500
 
     def show_info(self):
         display = f"""
@@ -12,8 +20,8 @@ class Client:
         Name: {self.name}
         Age: {self.age}
         Monthly Salary: ${self.monthly_salary}
-        Yearly Salary: ${self.monthly_salary * 12}
-        Loan Approved: {self.monthly_salary * 12 > 3500}
+        Yearly Salary: ${self.yearly_salary}
+        Loan Approved: {self.loan_approved}
         -------------------------------
         """
         print(display)
